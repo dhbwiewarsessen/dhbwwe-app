9cc430e38443f640b5627887eb6b26870ca904dc
oOWoWOo <40320189+oOWoWOo@users.noreply.github.com>
2018-10-13 11:28:08 +0200

    first try on layout
    added menuSpinner, ratingBar, tfAdditionalComment and btnSend

--- app/src/main/java/de/knusprig/dhbwiewarsessen/MainActivity.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/MainActivity.java
@@ -2,6 +2,8 @@ package de.knusprig.dhbwiewarsessen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] arraySpinner = new String[] {
+                "Menu 1", "Menu 2", "Menu 3"
+        };
+
+        Spinner menuSpinner = findViewById(R.id.menuSpinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//simple_spin‌​ner_dropdown_item);
+        menuSpinner.setAdapter(adapter);
+
     }
 }
