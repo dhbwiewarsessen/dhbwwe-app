b882e8b6b691aace83bc2a2168fd929fdb16b6f5
TvRXVII <TvRXVII@users.noreply.github.com>
2018-10-16 10:46:06 +0200

    Webhooktest

--- app/src/main/java/de/knusprig/dhbwiewarsessen/MainActivity.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
+//imports sind wichtig -Luis
 public class MainActivity extends AppCompatActivity {
 
     @Override
