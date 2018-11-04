bea7ae17e2da8cc236fc79a080b38d9252544ba1
polarode <40320189+polarode@users.noreply.github.com>
2018-11-04 17:57:59 +0100

    add Features

---
+++ app/src/androidTest/java/de/knusprig/dhbwiewarsessen/SignUpTest.java
@@ -0,0 +1,61 @@
+package de.knusprig.dhbwiewarsessen;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+@RunWith(AndroidJUnit4.class)
+public class SignUpTest {
+
+    @Rule public final ActivityTestRule<SignUpActivity> main = new ActivityTestRule<>(SignUpActivity.class);
+
+    @When("^User navigates to \"([^\"]*)\"$")
+    public void userNavigatesTo(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^User enters \"([^\"]*)\" into input field with id \"([^\"]*)\"$")
+    public void userEntersIntoInputFieldWithId(String text, String id) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^User clicks on Button with text \"([^\"]*)\"$")
+    public void userClicksOnButtonWithText(String text) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User should be logged in as \"([^\"]*)\"$")
+    public void userShouldBeLoggedInAs(String username) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User should see error \"([^\"]*)\"$")
+    public void userShouldSeeError(String errorText) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^User \"([^\"]*)\" is signed up$")
+    public void userIsSignedUp(String username) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^And User enters \"([^\"]*)\" into input field with id \"([^\"]*)\"$")
+    public void andUserEntersIntoInputFieldWithId(String text, String id) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
