a67388e7d61b10aa4757e6b63cade05ecb02ec98
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2018-11-06 10:31:55 +0100

    Add green-coffee
    autocomplete for .feature files no logner working

--- app/src/androidTest/java/de/knusprig/dhbwiewarsessen/RegisterSteps.java
+++ app/src/androidTest/java/de/knusprig/dhbwiewarsessen/RegisterSteps.java
@@ -1,67 +1,76 @@
 package de.knusprig.dhbwiewarsessen;
 
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+
+import com.mauriciotogneri.greencoffee.GreenCoffeeSteps;
+import com.mauriciotogneri.greencoffee.annotations.And;
+import com.mauriciotogneri.greencoffee.annotations.Given;
+import com.mauriciotogneri.greencoffee.annotations.Then;
+import com.mauriciotogneri.greencoffee.annotations.When;
 
 import org.junit.Rule;
-import org.junit.runner.RunWith;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+//import cucumber.api.java.en.And;
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.Then;
+//import cucumber.api.java.en.When;
 
-@RunWith(AndroidJUnit4.class)
-public class StepDefs {
 
-    @Rule public final ActivityTestRule<SignUpActivity> main = new ActivityTestRule<>(SignUpActivity.class);
+public class RegisterSteps extends GreenCoffeeSteps
+{
+    @Rule
+    public final ActivityTestRule<SignUpActivity> main = new ActivityTestRule<>(SignUpActivity.class);
 
     @When("^User navigates to \"([^\"]*)\"$")
     public void userNavigatesTo(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //throw new Exception();
     }
 
     @And("^User enters \"([^\"]*)\" into input field with id \"([^\"]*)\"$")
     public void userEntersIntoInputFieldWithId(String text, String id) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //throw new Exception();
     }
 
     @And("^User clicks on Button with text \"([^\"]*)\"$")
     public void userClicksOnButtonWithText(String text) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //throw new Exception();
     }
 
     @Then("^User should be logged in as \"([^\"]*)\"$")
     public void userShouldBeLoggedInAs(String username) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //throw new Exception();
     }
 
     @Then("^User should see error \"([^\"]*)\"$")
     public void userShouldSeeError(String errorText) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //throw new Exception();
     }
 
     @Given("^User \"([^\"]*)\" is registered$")
     public void userIsSignedUp(String username) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //throw new Exception();
     }
 
     @When("^And User enters \"([^\"]*)\" into input field with id \"([^\"]*)\"$")
     public void andUserEntersIntoInputFieldWithId(String text, String id) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //throw new Exception();
     }
 
     @Given("^Menu Entry exists for today$")
     public void menuEntryExistsForToday() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //throw new Exception();
     }
+
+    @Given("^I see an empty login form$")
+    public void iSeeAnEmptyLoginForm()
+    {
+    }
+
 }
---
+++ app/src/androidTest/java/de/knusprig/dhbwiewarsessen/RegisterTest.java
@@ -0,0 +1,42 @@
+package de.knusprig.dhbwiewarsessen;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.mauriciotogneri.greencoffee.GreenCoffeeConfig;
+import com.mauriciotogneri.greencoffee.GreenCoffeeTest;
+import com.mauriciotogneri.greencoffee.ScenarioConfig;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+
+@RunWith(Parameterized.class)
+public class RegisterTest extends GreenCoffeeTest
+{
+    @Rule
+    public ActivityTestRule activity = new ActivityTestRule<>(SignUpActivity.class);
+
+    public RegisterTest(ScenarioConfig scenarioConfig)
+    {
+        super(scenarioConfig);
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Iterable scenarios() throws IOException
+    {
+        return new GreenCoffeeConfig()
+                .withFeatureFromAssets("assets/register.feature")
+                .scenarios(
+                        //new Locale("en", "GB")
+                ); // the locales used to run the scenarios (optional)
+    }
+
+    @Test
+    public void test()
+    {
+        start(new RegisterSteps());
+    }
+}
