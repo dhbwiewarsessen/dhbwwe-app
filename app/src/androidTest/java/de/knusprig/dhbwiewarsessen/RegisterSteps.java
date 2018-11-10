9c65c23ecea04cdb6878470499a83c3779814bbb
Logaithmuz <40320189+Logarithmuz@users.noreply.github.com>
2018-11-10 18:13:17 +0100

    Add fragments for navigation drawer

--- app/src/androidTest/java/de/knusprig/dhbwiewarsessen/RegisterSteps.java
+++ app/src/androidTest/java/de/knusprig/dhbwiewarsessen/RegisterSteps.java
@@ -16,61 +16,60 @@ import org.junit.Rule;
 //import cucumber.api.java.en.When;
 
 
-public class RegisterSteps extends GreenCoffeeSteps
-{
+public class RegisterSteps extends GreenCoffeeSteps {
     @Rule
     public final ActivityTestRule<RegisterActivity> main = new ActivityTestRule<>(RegisterActivity.class);
 
     @When("^User navigates to \"([^\"]*)\"$")
     public void userNavigatesTo(String arg0) throws Throwable {
-        //throw new Exception();
+        throw new Exception();
     }
 
     @And("^User enters \"([^\"]*)\" into input field with id \"([^\"]*)\"$")
     public void userEntersIntoInputFieldWithId(String text, String id) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        //throw new Exception();
+        throw new Exception();
     }
 
     @And("^User clicks on Button with text \"([^\"]*)\"$")
     public void userClicksOnButtonWithText(String text) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        //throw new Exception();
+        throw new Exception();
     }
 
     @Then("^User should be logged in as \"([^\"]*)\"$")
     public void userShouldBeLoggedInAs(String username) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        //throw new Exception();
+        throw new Exception();
     }
 
     @Then("^User should see error \"([^\"]*)\"$")
     public void userShouldSeeError(String errorText) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        //throw new Exception();
+        throw new Exception();
     }
 
     @Given("^User \"([^\"]*)\" is registered$")
     public void userIsSignedUp(String username) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        //throw new Exception();
+        throw new Exception();
     }
 
     @When("^And User enters \"([^\"]*)\" into input field with id \"([^\"]*)\"$")
     public void andUserEntersIntoInputFieldWithId(String text, String id) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        //throw new Exception();
+        throw new Exception();
     }
 
     @Given("^Menu Entry exists for today$")
     public void menuEntryExistsForToday() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        //throw new Exception();
+        throw new Exception();
     }
 
     @Given("^I see an empty login form$")
-    public void iSeeAnEmptyLoginForm()
-    {
+    public void iSeeAnEmptyLoginForm() throws Throwable {
+        throw new Exception();
     }
 
 }
---
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/CreateRatingFragment.java
@@ -0,0 +1,40 @@
+package de.knusprig.dhbwiewarsessen;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class CreateRatingFragment extends Fragment {
+    
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_create_rating, container, false);
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        String[] items = new String[]{
+                "Menu 1", "Menu 2", "Menu 3"
+        };
+
+        Spinner menuSpinner = view.findViewById(R.id.menuSpinner);
+
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//simple_spin‌​ner_dropdown_item);
+        menuSpinner.setAdapter(adapter);
+    }
+}
+
