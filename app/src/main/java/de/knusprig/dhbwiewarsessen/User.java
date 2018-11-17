b0d6f0de4588c313d0196e15b5a7d823c227a796
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2018-11-17 01:30:15 +0100

    login and register

---
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/User.java
@@ -0,0 +1,47 @@
+package de.knusprig.dhbwiewarsessen;
+
+public class User {
+    String username;
+    String email;
+    String name;
+    String password;
+
+    public User(String username, String email, String name, String password) {
+        this.username = username;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/activities/RegisterRequest.java
@@ -0,0 +1,26 @@
+package de.knusprig.dhbwiewarsessen.activities;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+    private static final String REGISTER_REQUEST_URL = "dhbwwe.heliohost.org/Register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String name, String username, String email, String password, Response.Listener<String> listener) {
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("username", username);
+        params.put("name", name);
+        params.put("email", email);
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
