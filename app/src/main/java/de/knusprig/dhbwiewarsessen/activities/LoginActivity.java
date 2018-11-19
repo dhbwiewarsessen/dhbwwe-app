2a00295be37832c08a6b0cb9126cd761233ff82a
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2018-11-19 10:02:19 +0100

    Remove unused import

--- app/src/main/java/de/knusprig/dhbwiewarsessen/activities/LoginActivity.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/activities/LoginActivity.java
@@ -3,7 +3,6 @@ package de.knusprig.dhbwiewarsessen.activities;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
