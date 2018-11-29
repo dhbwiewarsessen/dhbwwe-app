f372fd662d44e2c9bc69870d81ca69180d986de9
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2018-11-29 15:24:29 +0100

    Update RetrieveMenu

--- app/src/main/java/de/knusprig/dhbwiewarsessen/httprequest/RetrieveMenuRequest.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/httprequest/RetrieveMenuRequest.java
@@ -7,13 +7,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RetrieveMenuRequest extends StringRequest {
-    private static final String REGISTER_REQUEST_URL = "https://dhbwwe.heliohost.org/RetrieveMenu.php";
+    private static final String RETRIEVE_MENU_REQUEST_URL = "https://dhbwwe.heliohost.org/RetrieveMenu.php";
     private Map<String, String> params;
 
-    public RetrieveMenuRequest(String id, Response.Listener<String> listener) {
-        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+    public RetrieveMenuRequest(String date, Response.Listener<String> listener) {
+        super(Method.GET, RETRIEVE_MENU_REQUEST_URL, listener, null);
         params = new HashMap<>();
-        params.put("id", id);
+        params.put("date", date);
     }
 
     @Override
