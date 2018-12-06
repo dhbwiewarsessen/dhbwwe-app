079e53ba1575655455c5eda27816acfe2ec1f8b5
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2018-12-06 14:22:13 +0100

    Revert "added getAllRatings method (in progress)"
    This reverts commit 0e2f475a5897ab86f04011b2f8f3398de226e8cd.

--- app/src/main/java/de/knusprig/dhbwiewarsessen/model/Rating.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/model/Rating.java
@@ -1,40 +1,7 @@
 package de.knusprig.dhbwiewarsessen.model;
 
 public class Rating {
-    private Dish dish;
-    private Double rating;
-    private String comment;
-    private int userId;
+    
 
-    public Dish getDish() {
-        return dish;
-    }
 
-    public void setDish(Dish dish) {
-        this.dish = dish;
-    }
-
-    public Double getRating() {
-        return rating;
-    }
-
-    public void setRating(Double rating) {
-        this.rating = rating;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
 }
