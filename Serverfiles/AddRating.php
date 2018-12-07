2352c809b93399806ca0ce75461053028574b755
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2018-12-07 17:29:41 +0100

    Update AddRating.php

--- Serverfiles/AddRating.php
+++ Serverfiles/AddRating.php
@@ -1,12 +1,15 @@
 <?php
     $con = mysqli_connect("localhost", "id7851303_dhbwiewarsessen", "jhgcwbncskijioihe", "id7851303_data");
 
-    $menu = $_POST["menu"];
+    $date = $_POST["date"];
+    $date_int = (int) preg_replace('/[^0-9]/', '', $date);//parse date to int for the query
+    $dish = $_POST["dish"];
     $rating = $_POST["rating"];
     $comment = $_POST["comment"];
     $userId = $_POST["userId"];
-    $statement = mysqli_prepare($con, "INSERT INTO ratings (menu, rating, comment, userId) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "ssss", $menu, $rating, $comment, $userId);
+
+    $statement = mysqli_prepare($con, "INSERT INTO ratings (date, dish, rating, comment, user_id) VALUES (?, ?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "isisi", $date_int, $dish, $rating, $comment, $userId);
     mysqli_stmt_execute($statement);
 
     $response = array();
