5a65929eaeb6afcff7517440799b81be522b0322
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2018-12-11 15:32:06 +0100

    tiny adjustments

--- app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/CreateRatingFragment.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/CreateRatingFragment.java
@@ -1,5 +1,6 @@
 package de.knusprig.dhbwiewarsessen.controller.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,13 +14,28 @@ import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import de.knusprig.dhbwiewarsessen.R;
+import de.knusprig.dhbwiewarsessen.controller.activities.MainActivity;
+import de.knusprig.dhbwiewarsessen.httprequest.CreateRatingRequest;
+import de.knusprig.dhbwiewarsessen.model.Menu;
 
 public class CreateRatingFragment extends Fragment {
+
+    private MainActivity main;
+    private Menu menu;
 
     private EditText comment;
     private Spinner menuSpinner;
     private RatingBar ratingBar;
+
+    private String[] items;
 
     @Nullable
     @Override
@@ -46,83 +62,62 @@ public class CreateRatingFragment extends Fragment {
 
 //      Set action listener
         Button btnSendRating = (Button) view.findViewById(R.id.btnSend);
-        btnSendRating.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptAddRating();
-            }
-        });
 
         ArrayAdapter adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//simple_spin‌​ner_dropdown_item);
         menuSpinner.setAdapter(adapter);
     }
 
-    public void attemptAddRating() {
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+        items = new String[]{
+                menu.getDishes().get(0).getTitle(),
+                menu.getDishes().get(1).getTitle(),
+                menu.getDishes().get(2).getTitle()
+        };
+    }
+
+    public void setMain(MainActivity main) {
+        this.main = main;
+    }
+
+    public void attemptAddRating(View view) {
+        final String userId = "" + main.getCurrentUser().getUserId();
         final String comment = this.comment.getText().toString(); //if comment is just whitespaces put "null" into database
-        final String selectedMenu = menuSpinner.getSelectedItem().toString();
-        final String rating = ratingBar.getRating()+"";
+        final String selectedDish = menuSpinner.getSelectedItem().toString();
+        final String rating = ((int) (ratingBar.getRating()*10))+"";
+        final String date="";//todo set date
         System.out.println("Rating: " + rating);
-        System.out.println("Selected menu: " + selectedMenu);
+        System.out.println("Selected menu: " + selectedDish);
         System.out.println("Additional comment: " + comment);
 
-        boolean cancel = false;
-        View focusView = null;
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
 
-//        if (cancel) {
-//            // There was an error; don't attempt login and focus the first
-//            // form field with an error.
-//            focusView.requestFocus();
-//        } else {
-//            Response.Listener<String> responseListener = new Response.Listener<String>() {
-//                @Override
-//                public void onResponse(String response) {
-//                    try {
-//                        JSONObject jsonResponse = new JSONObject(response);
-//                        boolean success = jsonResponse.getBoolean("success");
-//
-//                        if (success) {
-//                            Intent data = new Intent();
-//                            data.putExtra("username", username);
-//                            data.putExtra("password", password);
-//                            data.putExtra("name", name);
-//                            data.putExtra("email", email);
-//
-//
-//                            setResult(RESULT_OK, data);
-//                            finish();
-//                        } else {
-//                            try {
-//                                System.out.println("no success");
-//                                String error = jsonResponse.getString("error");
-//                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-//                                builder.setMessage("Register Failed:\n"+error)
-//                                        .setNegativeButton("Retry", null)
-//                                        .create()
-//                                        .show();
-//                            } catch (Exception e) {
-//                                System.out.println("Couldn't create Error Message 'Register failed'");
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                    } catch (JSONException e) {
-//                        e.printStackTrace();
-//                        String error = "There is a problem with the DB Server";
-//                        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-//                        builder.setMessage("Register Failed:\n"+error)
-//                                .setNegativeButton("Retry", null)
-//                                .create()
-//                                .show();
-//                    }
-//                }
-//            };
-//
-//            RegisterRequest registerRequest = new RegisterRequest(name, username, email, password, responseListener);
-//            RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
-//            queue.add(registerRequest);
-//        }
+                    if (success) {
+                        //todo do something
+                    } else {
+                        //todo do something else
+                    }
 
+                } catch (JSONException e) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(main.getApplicationContext());
+                    builder.setMessage("JSON Exception")
+                            .setNegativeButton("Retry", null)
+                            .create()
+                            .show();
+                    e.printStackTrace();
+                }
+            }
+        };
 
+        CreateRatingRequest createRatingRequest = new CreateRatingRequest(userId, selectedDish, date, rating, comment, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(main.getApplicationContext());
+        queue.add(createRatingRequest);
     }
 }
 
