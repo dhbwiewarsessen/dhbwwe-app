168dcc2ae58c616f9ee69513440ae33038e7e81e
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2018-12-12 16:29:11 +0100

    added delete rating feature
    ratings can be deleted with a long click

--- app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/UserRatingFragment.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/UserRatingFragment.java
@@ -1,12 +1,16 @@
 package de.knusprig.dhbwiewarsessen.controller.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -56,6 +60,25 @@ public class UserRatingFragment extends Fragment{
         ratingAdapter = new RatingAdapter(getActivity(),listRating);
 
         listView.setAdapter(ratingAdapter); //Displaying the list in the listView
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //todo open dialog to choose between edit and delete
+                final int pos = position;
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Do you want to delete this entry?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                listRating.remove(pos);
+                                listView.invalidateViews();
+                            }
+                        }).setNegativeButton("No", null)
+                        .create()
+                        .show();
+                return false;
+            }
+        });
     }
 
     public void setMainActivity(MainActivity mainActivity){
