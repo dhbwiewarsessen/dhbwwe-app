6b58f40912c402b50720cff7ec6fae032476088b
polarode <40320189+polarode@users.noreply.github.com>
2019-05-03 16:14:16 +0200

    Create TestAll.java

---
+++ app/src/androidTest/java/de/knusprig/dhbwiewarsessen/unitTests/TestAll.java
@@ -0,0 +1,9 @@
+package de.knusprig.dhbwiewarsessen.unitTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ LoginUnitTest.class, CreateRatingUnitTest.class})
+public class TestAll {
+}
