b2a023a00a8388fcca8dcbbf3bc7899422486abd
DrHaid <40427630+DrHaid@users.noreply.github.com>
2019-05-08 11:45:53 +0200

    Email confirmation appears to be working

---
+++ Serverfiles/EmailConfirmation.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Email Confirmation</title>
+</head>
+
+<body>
+    <img src="https://raw.githubusercontent.com/TvRXVII/DHBWieWarsEssen/master/Logo/Logo.png" alt="logo" width="300">
+    </br>
+    <h3>Your email has been successfully confirmed.</h3>
+    <img src="https://i.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.webp" alt="i shid and fard">
+</body>
+</html>
