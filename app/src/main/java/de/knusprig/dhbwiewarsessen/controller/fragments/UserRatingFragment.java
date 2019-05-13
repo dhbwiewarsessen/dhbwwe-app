728bd471c7b1eaaf0ad73b2b796947d81c552086
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2019-05-13 08:36:40 +0200

    Update UserRatingFragment.java

--- app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/UserRatingFragment.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/UserRatingFragment.java
@@ -71,7 +71,7 @@ public class UserRatingFragment extends Fragment {
             pullToRefresh.setRefreshing(false);
         });
 
-        listRating.sort(Comparator.comparing(Rating::getDate)); //Default sorting
+        //listRating.sort(Comparator.comparing(Rating::getDate)); //Default sorting
 
         ArrayAdapter<String> adapterS = new ArrayAdapter<>(getActivity(),android.R.layout.simple_spinner_item, mainActivity.getDefValues());
         adapterS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -159,11 +159,11 @@ public class UserRatingFragment extends Fragment {
                 //TODO: add name sorting
                 break;
             case "Dish":
-                listRating.sort(Comparator.comparing(Rating::getDish));
+                //listRating.sort(Comparator.comparing(Rating::getDish));
 
                 break;
             case "Date":
-                listRating.sort(Comparator.comparing(Rating::getDate).reversed());
+                //listRating.sort(Comparator.comparing(Rating::getDate).reversed());
                 Collections.sort(listRating, (o1, o2) -> o2.getDate().compareTo(o1.getDate()));
                 break;
         }
