fa8442ae28e35bd99d32b9896dcdf1347bf51a57
Banerry <cracky_x3@hotmail.com>
2019-05-15 09:18:58 +0200

    filtering by ratings works now

--- app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/RatingsFragment.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/RatingsFragment.java
@@ -173,19 +173,15 @@ public class RatingsFragment extends Fragment {
     public void sortBySpinner(String sortBy){
         switch (sortBy){
             case "Name":
-                //filteredListRating.sort(Comperator.comparing(Rating::getUsername));
                 Collections.sort(filteredListRating, ((o1, o2) -> o1.getUsername().compareTo(o2.getUsername())));
                 break;
             case "Dish":
-                //filteredListRating.sort(Comparator.comparing(Rating::getDish));
                 Collections.sort(filteredListRating, ((o1, o2) -> o1.getDish().compareTo(o2.getDish())));
                 break;
             case "Rating":
-                //filteredListRating.sort(Comparator.comparing(Rating::getDish));
-                Collections.sort(filteredListRating, ((o1, o2) -> o1.getDish().compareTo(o2.getDish())));
+                Collections.sort(filteredListRating, ((o1, o2) -> o2.getStringRating().compareTo(o1.getStringRating())));
                 break;
             case "Date":
-                //filteredListRating.sort(Comparator.comparing(Rating::getDate).reversed());
                 Collections.sort(filteredListRating, (o1, o2) -> o2.getDate().compareTo(o1.getDate()));
                 break;
         }
@@ -209,8 +205,10 @@ public class RatingsFragment extends Fragment {
                  }
                 break;
             case "Rating":
+                double sDouble = Double.parseDouble(s.toString()) * 10;
+                s = ((int) (sDouble)) +"";
                 for(Rating r : listRating){
-                    if (r.getStringRating().toLowerCase().contains(s)){
+                    if (r.getStringRating().toLowerCase().equals(s)){
                         filteredListRating.add(r);
                     }
                 }
