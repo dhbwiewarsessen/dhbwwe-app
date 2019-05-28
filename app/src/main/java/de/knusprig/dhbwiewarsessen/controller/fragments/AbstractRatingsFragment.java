8571416102e6c1503c0ac0d2490134151cf98bcc
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2019-05-28 12:00:46 +0200

    Removed dublicate code

--- app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/AbstractRatingsFragment.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/AbstractRatingsFragment.java
@@ -34,10 +34,92 @@ public abstract class AbstractRatingsFragment extends Fragment {
     protected Spinner filterSpinner;
     protected ListView listView;
     protected List<Rating> filteredListRating;
+    protected RatingAdapter ratingAdapter;
+    protected SwipeRefreshLayout pullToRefresh;
+    protected EditText filterText;
+    protected String currentSpinnerItem;
+    protected List<String> defValues = new ArrayList<>();
 
-    public abstract View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState);
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user_rating, container, false);
+    }
 
-    public abstract void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState);
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        filteredListRating = new ArrayList<>();
+        filteredListRating.addAll(listRating);
+
+        listView = view.findViewById(R.id.rating_list);
+        filterSpinner = view.findViewById(R.id.filterSpinner);
+        filterText = view.findViewById(R.id.filterText);
+
+        initDefValues();
+
+
+        filterText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                System.out.println("changed");
+                if(s.length() > 0){
+                    System.out.println(s);
+                    filterByText(s.toString().toLowerCase(), currentSpinnerItem);
+                }
+                else{
+                    filteredListRating.clear();
+                    filteredListRating.addAll(listRating);
+                    refreshList();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        pullToRefresh = view.findViewById(R.id.pullToRefresh);
+        pullToRefresh.setOnRefreshListener(() -> {
+            mainActivity.refeshDataFromServer();
+            //when new data is fetched
+            refreshList();
+            //when request is done
+            pullToRefresh.setRefreshing(false);
+        });
+
+        //listRating.sort(Comparator.comparing(Rating::getDate)); //Default sorting
+
+        ArrayAdapter<String> adapterS = new ArrayAdapter<>(getActivity(),android.R.layout.simple_spinner_item, defValues);
+        adapterS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        filterSpinner.setAdapter(adapterS);
+
+        filterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                sortBySpinner(defValues.get(position), filteredListRating);
+                currentSpinnerItem = defValues.get(position);
+                refreshList();
+                filterText.setHint("Filter by: " + currentSpinnerItem);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //TODO add error
+            }
+        });
+        ratingAdapter = new RatingAdapter(getActivity(), filteredListRating);
+
+        listView.setAdapter(ratingAdapter); //Displaying the list in the listView
+        listView.setOnItemLongClickListener(createLongClickListener());
+    }
+
+    protected abstract AdapterView.OnItemLongClickListener createLongClickListener();
 
     public void setMainActivity(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
@@ -64,9 +146,13 @@ public abstract class AbstractRatingsFragment extends Fragment {
         }
     }
 
-    public abstract void initDefValues();
-
-    public abstract void filterByText(CharSequence s, String sortBy);
+    public void initDefValues() {
+        if (defValues.size() == 0) {
+            defValues.add("Date");
+            defValues.add("Dish");
+            defValues.add("Rating");
+        }
+    }
 
     public void refreshList() {
         try {
@@ -77,5 +163,42 @@ public abstract class AbstractRatingsFragment extends Fragment {
         }
     }
 
+    public void filterByText(CharSequence s, String sortBy){
+        filteredListRating.clear();
+        switch (sortBy){
+            case "Name":
+                for(Rating r : listRating){
+                    if(r.getUsername().toLowerCase().contains(s)){
+                        filteredListRating.add(r);
+                    }
+                }
+                break;
+            case "Dish":
+                for(Rating r : listRating){
+                    if(r.getDish().toLowerCase().contains(s)){
+                        filteredListRating.add(r);
+                    }
+                }
+                break;
+            case "Rating":
+                double sDouble = Double.parseDouble(s.toString()) * 10;
+                s = ((int) (sDouble)) +"";
+                for(Rating r : listRating){
+                    if (r.getStringRating().toLowerCase().equals(s)){
+                        filteredListRating.add(r);
+                    }
+                }
+
+                break;
+            case "Date":
+                for(Rating r : listRating){
+                    if(r.getStringDate().contains(s)){
+                        filteredListRating.add(r);
+                    }
+                }
+                break;
+        }
+        refreshList();
+    }
 
 }
--- app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/UserRatingFragment.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/UserRatingFragment.java
@@ -34,87 +34,9 @@ import de.knusprig.dhbwiewarsessen.model.RatingAdapter;
 
 public class UserRatingFragment extends AbstractRatingsFragment {
 
-    private RatingAdapter ratingAdapter;
-    private SwipeRefreshLayout pullToRefresh;
-    private EditText filterText;
-    private String currentSpinnerItem;
-    private List<String> defValues = new ArrayList<>();
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user_rating, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        filteredListRating = new ArrayList<>();
-        filteredListRating.addAll(listRating);
-
-        listView = view.findViewById(R.id.rating_list);
-        filterSpinner = view.findViewById(R.id.filterSpinner);
-        filterText = view.findViewById(R.id.filterText);
-
-        initDefValues();
-
-        filterText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                System.out.println("changed");
-                if (s.length() > 0) {
-                    System.out.println(s);
-                    filterByText(s.toString().toLowerCase(), currentSpinnerItem);
-                } else {
-                    filteredListRating.clear();
-                    filteredListRating.addAll(listRating);
-                    refreshList();
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        pullToRefresh = view.findViewById(R.id.pullToRefresh);
-        pullToRefresh.setOnRefreshListener(() -> {
-            mainActivity.refeshDataFromServer();
-            //when new data is fetched
-            refreshList();
-            //when request is done
-            pullToRefresh.setRefreshing(false);
-        });
-
-        //listRating.sort(Comparator.comparing(Rating::getDate)); //Default sorting
-
-        ArrayAdapter<String> adapterS = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, defValues);
-        adapterS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        filterSpinner.setAdapter(adapterS);
-
-        filterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                sortBySpinner(defValues.get(position), filteredListRating);
-                currentSpinnerItem = defValues.get(position);
-                refreshList();
-                filterText.setHint("Filter by: " + currentSpinnerItem);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                //TODO add error
-            }
-        });
-        ratingAdapter = new RatingAdapter(getActivity(), filteredListRating);
-
-        listView.setAdapter(ratingAdapter); //Displaying the list in the listView
-        listView.setOnItemLongClickListener((parent, view1, position, id) -> {
+    protected AdapterView.OnItemLongClickListener createLongClickListener() {
+        return (parent, view1, position, id) -> {
             final int pos = position;
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage("Choose action for this rating")
@@ -158,55 +80,6 @@ public class UserRatingFragment extends AbstractRatingsFragment {
                     .create()
                     .show();
             return false;
-        });
+        };
     }
-
-    @Override
-    public void initDefValues() {
-        if (defValues.size() == 0) {
-            defValues.add("Date");
-            defValues.add("Dish");
-            defValues.add("Rating");
-        }
-    }
-
-    public void filterByText(CharSequence s, String sortBy) {
-        filteredListRating.clear();
-        switch (sortBy) {
-            case "Name":
-                for (Rating r : listRating) {
-                    if (r.getUsername().toLowerCase().contains(s)) {
-                        filteredListRating.add(r);
-                    }
-                }
-                break;
-            case "Dish":
-                for (Rating r : listRating) {
-                    if (r.getDish().toLowerCase().contains(s)) {
-                        filteredListRating.add(r);
-                    }
-                }
-                break;
-            case "Rating":
-                double sDouble = Double.parseDouble(s.toString()) * 10;
-                s = ((int) (sDouble)) + "";
-                for (Rating r : listRating) {
-                    if (r.getStringRating().toLowerCase().equals(s)) {
-                        filteredListRating.add(r);
-                    }
-                }
-
-                break;
-            case "Date":
-                for (Rating r : listRating) {
-                    if (r.getStringDate().contains(s)) {
-                        filteredListRating.add(r);
-                    }
-                }
-                break;
-        }
-        refreshList();
-    }
-
-
 }
