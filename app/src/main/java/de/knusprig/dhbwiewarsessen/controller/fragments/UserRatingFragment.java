40f8b5683f70e358151d6653e0c4d419cc79e77d
polarode <40320189+polarode@users.noreply.github.com>
2019-05-28 12:44:48 +0200

    Update UserRatingFragment.java

--- app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/UserRatingFragment.java
+++ app/src/main/java/de/knusprig/dhbwiewarsessen/controller/fragments/UserRatingFragment.java
@@ -1,20 +1,8 @@
 package de.knusprig.dhbwiewarsessen.controller.fragments;
 
 import android.app.AlertDialog;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -23,14 +11,8 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import de.knusprig.dhbwiewarsessen.R;
-import de.knusprig.dhbwiewarsessen.controller.activities.MainActivity;
 import de.knusprig.dhbwiewarsessen.httprequest.DeleteRatingRequest;
 import de.knusprig.dhbwiewarsessen.model.Rating;
-import de.knusprig.dhbwiewarsessen.model.RatingAdapter;
 
 public class UserRatingFragment extends AbstractRatingsFragment {
 
@@ -68,7 +50,7 @@ public class UserRatingFragment extends AbstractRatingsFragment {
                                     System.out.println("Couldn't delete rating from server");
                                 }
                             } catch (JSONException e) {
-                                e.printStackTrace();
+                                Log.e("deleteRating", "JSONExeption on deleting a rating", e);
                                 Toast.makeText(mainActivity.getApplicationContext(), "JSON Error", Toast.LENGTH_LONG).show();
                             }
                         });
